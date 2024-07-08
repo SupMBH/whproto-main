e62b71c21ad3cab1ebfa932810ace2857257a5f9
supmbh <supmbh@gmail.com>
2024-07-08 09:58:37 +0200

    creation page des employés avec installation react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import EmployeeForm from './components/EmployeeForm';
+import EmployeeListPage from './pages/EmployeeListPage';
 import './styles/index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to WHproto</h1>
-      </header>
-      <EmployeeForm />
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to WHproto</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/employees">View Current Employees</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route exact path="/" element={<EmployeeForm />} />
+          <Route path="/employees" element={<EmployeeListPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/EmployeeListPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import EmployeeList from '../components/EmployeeList';
+
+const EmployeeListPage = () => {
+  return (
+    <div>
+      <h1>HRnet</h1>
+      <EmployeeList />
+    </div>
+  );
+};
+
+export default EmployeeListPage;
